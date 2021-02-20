79468f654395f21d9a253f55497e735c2984d5c5
SNsaid1998 <61479527+SNsaid1998@users.noreply.github.com>
2021-02-19 19:07:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -35,10 +35,25 @@ const layer = new CSVLayer({
           type: "simple-marker", 
           color:"red"
         };
-
-      csvLayer.renderer = {
-        type: "simple", // autocasts as new SimpleRenderer()
-        symbol: symbol
+ csvLayer.renderer = {
+        type: "heatmap", 
+        colorStops: [
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#9C33FF", ratio: 0.083 },
+            { color: "#6B33FF", ratio: 0.166 },
+            { color: "#3386FF",ratio: 0.249 },
+            { color: "#33CAFF", ratio: 0.332 },
+            { color: "#33FFEC", ratio: 0.415 },
+            { color: "#33FFAF", ratio: 0.498 },
+            { color: "#42FF33", ratio: 0.581 },
+            { color: "#96FF33", ratio: 0.664 },
+            { color: "#E0FF33", ratio: 0.747 },
+            { color: "#FFE633", ratio: 0.83 },
+            { color: "#FFA233", ratio: 0.913 },
+            { color: "#FF3333", ratio: 1 }
+          ],
+          maxPixelIntensity: 400,
+          minPixelIntensity: 0
       };
 
       var map = new Map({
